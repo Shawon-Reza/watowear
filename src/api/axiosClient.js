2d54c2a0f4299552a28a28f857dc57b6a8304ac7
Samin Yasar <yasarsamin57@gmail.com>
2025-12-26 16:09:21 +0600

    feat: redesign analytics dashboard stats cards with dynamic rendering and add admin header

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-	baseURL: "http://10.10.13.75:8000",
-	// baseURL: "https://endlessly-unified-guppy.ngrok-free.app",
+	// baseURL: "http://10.10.13.75:8000",
+	baseURL: "https://endlessly-unified-guppy.ngrok-free.app",
 });
 
 axiosClient.interceptors.request.use(
